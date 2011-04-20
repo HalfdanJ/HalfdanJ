09b37e4187cbc03a0e4c2d700d9c9fd8d0a1d8a3
Jonas Jongejan <jonas@halfdanj.dk>
2011-04-20 21:10:53 +0200

    Added Se Min Skygge and empty example

---
+++ Se Min Skygge/Plugins/VideoPlayer/VideoPlayer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Plugin.h"
+#import <netinet/in.h>
+#import <sys/socket.h>
+#import <SystemConfiguration/SystemConfiguration.h>
+
+//#import "BSender.h"
+//#import "BReceiver.h"
+#import <QTKit/QTKit.h>
+
+//#import "ArduinoController.h"
+
+@class QTMovie;
+#define NUMVIDEOS 1
+
+@interface VideoPlayer : ofPlugin {
+	QTMovie     		*movie[NUMVIDEOS];
+	QTVisualContextRef	textureContext[NUMVIDEOS];
+	CVOpenGLTextureRef  currentFrame[NUMVIDEOS];
+	NSSize sizes[NUMVIDEOS];
+	
+	int lastFramesVideo;
+	BOOL forceDrawNextFrame;
+//	ArduinoController * arduino;
+	NSMutableArray * loadedFiles;
+	IBOutlet NSArrayController * loadedFilesController;
+	BOOL cancelReboot;
+	
+	IBOutlet NSPopUpButton * videoSelector;
+	IBOutlet NSPopUpButton * chapterSelector;
+
+}
+
+@property (readwrite, retain) NSMutableArray * loadedFiles;
+-(IBAction) restart:(id)sender;
+@end
---
+++ Se Min Skygge/src/AppController.mm
@@ -0,0 +1,39 @@
+//
+//  AppController.m
+//
+//  Created by Jonas Jongejan on 03/11/09.
+//
+
+#import "AppController.h"
+#include "PluginIncludes.h"
+#include "testApp.h"
+#include "ofAppCocoaWindow.h"
+
+extern testApp * OFSAptr;
+extern ofAppBaseWindow * window;
+
+@implementation AppController
+
+-(void) setupApp{
+	[pluginManagerController setNumberOutputViews:1];	
+}
+
+
+-(void) awakeFromNib {
+	baseApp = OFSAptr;
+	cocoaWindow = window;	
+	((ofAppCocoaWindow*)cocoaWindow)->windowController = self;
+	
+	ofSetBackgroundAuto(false);
+	
+}
+
+-(void) setupPlugins{
+	[pluginManagerController addHeader:@"Plugins"];
+	//[pluginManagerController addPlugin:[[Parsifal alloc] init]];
+	//[pluginManagerController addPlugin:[[VideoPlayer alloc] init]];
+	[pluginManagerController addPlugin:[[Keystoner alloc] initWithSurfaces:[NSArray arrayWithObjects:@"Screen", nil]]];
+
+}
+
+@end
