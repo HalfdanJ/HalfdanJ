023321968af7f69a3061cfd2a93169e5ea3dae31
HalfdanJ / ZeBU <jonas@halfdanj.dk>
2011-06-17 16:02:31 +0200

    Wall

--- Se Min Skygge/Plugins/InteractiveWall/InteractiveWall.h
+++ Se Min Skygge/Plugins/InteractiveWall/InteractiveWall.h
@@ -11,6 +11,9 @@ struct bar {
 
 @interface InteractiveWall : ofPlugin {
     vector< bar > bars;
+    vector< bar > bars2;
+
+    bar leftBar, rightBar;
 //    BlobTrackerInstance2d * tracker;
     KinectInstance * kinect;
 }
