13a19942df2930f77f2161a02cd1555f2b76abf5
Jonas Jongejan <jonas@halfdanj.dk>
2011-08-29 10:09:30 +0200

    added mtc support for shadows

--- Se Min Skygge/Plugins/Shadows/Shadows.mm
+++ Se Min Skygge/Plugins/Shadows/Shadows.mm
@@ -11,6 +11,7 @@
 #import "Keystoner.h"
 #import "RenderEngine.h"
 #include "TextureGrid.h"
+#import "Midi.h"
 
 @implementation Shadows
 @synthesize timeline;
@@ -39,13 +40,14 @@
     [self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0 minValue:0 maxValue:10] named:@"timelineTime"];
     [self addProperty:[NumberProperty sliderPropertyWithDefaultvalue:0 minValue:0 maxValue:10] named:@"inputGain"];
     
-    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"useOpenCV"];
-    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"useShaders"];
+    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:NO] named:@"useOpenCV"];
+    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:NO] named:@"useShaders"];
     
-    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"reloadShaders"];
-    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"reloadImages"];
+    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:NO] named:@"reloadShaders"];
+    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:NO] named:@"reloadImages"];
     
-    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:0.0] named:@"debug"];
+    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:NO] named:@"debug"];
+    [self addProperty:[BoolProperty boolPropertyWithDefaultvalue:YES] named:@"midiMTC"];
     
     bufferIndex = 0;
     bufferFill = 0;
@@ -53,14 +55,14 @@
     [self assignMidiChannel:6]; 
     
     [self loadTimeline];
-
+    
     
 }
 
 -(void) loadTimeline{
     NSString * path = [NSString stringWithCString:ofToDataPath("timeline.txt",true).c_str() encoding:NSUTF8StringEncoding];
     [self setTimeline:[NSString stringWithContentsOfURL:[NSURL fileURLWithPath:path isDirectory:NO] encoding:NSUTF8StringEncoding error:nil]];
-
+    
     NSRange _start = [timeline rangeOfString:@"	Frame	seconds	"];
     NSRange _stop = [timeline rangeOfString:@"End of Keyframe Data"];
     
@@ -72,7 +74,7 @@
     [self setTimeline:substring];
     
     NSArray *listItems = [substring componentsSeparatedByString:@"	"];
-
+    
     int i=0;
     for(NSString * str in listItems){
         NSLog(@"%i %i %@",i,i%3,str);
@@ -89,7 +91,7 @@
     for(int i=0;i<keyframes.size();i++){
         cout<<"keyframe "<<keyframes[i].time<<"-"<<keyframes[i].value<<endl;
     }
-
+    
 }
 
 -(void)setup{
@@ -104,16 +106,16 @@
         
         history[i] = new ofxFBOTexture();
         history[i]->allocate(640, 480);
-     //   history[i]->clear(0,0,0,1);
+        //   history[i]->clear(0,0,0,1);
     }
-      
-  /*  grayOutputImage = new ofxCvGrayscaleImage();
-    grayOutputImage->allocate(1024,768);
-    grayOutputImage->set(255);
-    grayOutputImageTemp = new ofxCvGrayscaleImage();
-    grayOutputImageTemp->allocate(1024,768);
-    grayOutputImageTemp->set(255);
-    */
+    
+    /*  grayOutputImage = new ofxCvGrayscaleImage();
+     grayOutputImage->allocate(1024,768);
+     grayOutputImage->set(255);
+     grayOutputImageTemp = new ofxCvGrayscaleImage();
+     grayOutputImageTemp->allocate(1024,768);
+     grayOutputImageTemp->set(255);
+     */
     
     lightImage = new ofxCvColorImage();
     lightImage->allocate(1024,768);
@@ -130,7 +132,7 @@
     lightFlashImage = new ofImage();
     lightFlashImage->loadImage([file cStringUsingEncoding:NSUTF8StringEncoding]);
     
- 
+    
     output = new ofxCvColorImage();
     output->allocate(1024,768);
     
@@ -139,10 +141,10 @@
     
     motionblurFbo[0] = new ofxFBOTexture();
     motionblurFbo[0]->allocate(1024,768);
-  //  motionblurFbo[0]->clear(0,0,0,1);
+    //  motionblurFbo[0]->clear(0,0,0,1);
     motionblurFbo[1] = new ofxFBOTexture();
     motionblurFbo[1]->allocate(1024,768);
-  //  motionblurFbo[1]->clear(0,0,0,1);
+    //  motionblurFbo[1]->clear(0,0,0,1);
     
     blur = new shaderBlur();
     blur->setup(400,300);
@@ -150,11 +152,22 @@
     blurHist = new shaderBlur();
     blurHist->setup(400,300);
     
-  
+    
     
 }
 
 -(void)update:(NSDictionary *)drawingInformation{
+    if(PropB(@"midiMTC")){
+        [Prop(@"timelineTime") setFloatValue:[GetPlugin(Midi) getMTCSeconds]];
+    }
+    
+    float timelineTime = PropF(@"timelineTime");
+    float valueTime = [self valueForTime:timelineTime];
+    if(timelineTime > 0 && valueTime != -1000){
+        float index = (-valueTime * ofGetFrameRate()) / BUFFER_SIZE;
+        [Prop(@"currentIndex") setFloatValue:1-index];
+    }
+    
     if(PropB(@"reloadShaders")){
         [Prop(@"reloadShaders") setBoolValue:0];
         //        shader = new ofxShader();
@@ -213,9 +226,9 @@
      //  *buffer[bufferIndex] = *trackerImageRef;
      
      */
-     float speed = PropF(@"playbackSpeed");
-     [Prop(@"currentIndex") setFloatValue:ofClamp(PropF(@"currentIndex")+speed*1.0/BUFFER_SIZE, 0, 1)];
-     
+    float speed = PropF(@"playbackSpeed");
+    [Prop(@"currentIndex") setFloatValue:ofClamp(PropF(@"currentIndex")+speed*1.0/BUFFER_SIZE, 0, 1)];
+    
 }
 
 -(float)valueForTime:(float)time{
@@ -233,7 +246,7 @@
     
     if(l.time == -1 || r.time == -1){
         cout<<"No timeline for time "<<time<<endl;
-        return 0;
+        return -1000;
     } 
     
     float p = (time - l.time) / (r.time-l.time);
@@ -242,7 +255,7 @@
 }
 
 -(void)controlDraw:(NSDictionary *)drawingInformation{
- /*  if([[GetPlugin(Kinect) enabled] boolValue] && [kinect kinectConnected]){
+    if([[GetPlugin(Kinect) enabled] boolValue] && [kinect kinectConnected]){
         ofBackground(0, 0, 0);
         ofEnableAlphaBlending();
         
@@ -303,45 +316,44 @@
             
             
         }glPopMatrix();
-        }
-    
-        //Real timeline (!)
-        glPushMatrix();{
-            glTranslated(5, 180, 0);
-            
-            ofNoFill();
-            ofSetColor(150, 150, 150);
-            ofRect(0,0,310,70);
-            
-            if(keyframes.size() > 1){
-                glPushMatrix();
-                glTranslated(0,70,0);
-                
-                float d = keyframes[keyframes.size()-1].time - keyframes[0].time;
-                
-                glColor3d(0,0,255);
-                glBegin(GL_LINE_STRIP);
-                for(int i=0;i<keyframes.size();i++){
-                    glVertex2d(309.0 * keyframes[i].time/d, 70.0 * keyframes[i].value/10.0);
-                }
-                glEnd();
-
-                glPopMatrix();
-                
-                ofSetColor(0, 255, 0);
-                
-                float x = PropF(@"timelineTime")*309.0/d;
-                ofLine(x, 0, x, 70);
-                ofCircle(x, [self valueForTime:PropF(@"timelineTime")]*7.0+70, 4);
-                
-            }
-            
-                        
-        }glPopMatrix();
-        
-             
     }
-   */
+    
+    //Real timeline (!)
+    glPushMatrix();{
+        glTranslated(5, 180, 0);
+        
+        ofNoFill();
+        ofSetColor(150, 150, 150);
+        ofRect(0,0,310,70);
+        
+        if(keyframes.size() > 1){
+            glPushMatrix();
+            glTranslated(0,70,0);
+            
+            float d = keyframes[keyframes.size()-1].time - keyframes[0].time;
+            
+            glColor3d(0,0,255);
+            glBegin(GL_LINE_STRIP);
+            for(int i=0;i<keyframes.size();i++){
+                glVertex2d(309.0 * keyframes[i].time/d, 70.0 * keyframes[i].value/10.0);
+            }
+            glEnd();
+            
+            glPopMatrix();
+            
+            ofSetColor(0, 255, 0);
+            
+            float x = PropF(@"timelineTime")*309.0/d;
+            ofLine(x, 0, x, 70);
+            ofCircle(x, [self valueForTime:PropF(@"timelineTime")]*7.0+70, 4);
+            
+        }
+        
+        
+    }glPopMatrix();
+    
+    
+
 }
 
 -(void)draw:(NSDictionary *)drawingInformation{
@@ -385,7 +397,7 @@
             
             TextureGrid texGrid;
             texGrid.drawTextureGrid(tex,  poly, corners, 10);
-     
+            
             
             if(useShader){
                 shader.end();
@@ -453,7 +465,7 @@
             ofSetColor(255, 255, 255,255);
             //        blur->draw(0.0,0,1,1,true);
             // ofRect(0.1, 0.1, 0.8, 0.8);
-
+            
             //blur->draw(0,0,1,1,false);
             int i = [self getCurrentBufferIndex];    
             //cout<<"Draw "<<i<<endl;
@@ -499,81 +511,81 @@
                     ofSetColor(255,255,255,255.0*powf(PropF(@"back"),1.0/2.2));                
                 }
                 lightImage->draw(0,0,Aspect(@"Screen",1),1);
-          //      ofRect(0,0,1,1);        
+                //      ofRect(0,0,1,1);        
                 glBlendFunc(GL_ZERO, GL_ONE_MINUS_SRC_COLOR);     
-               motionblurFbo[pingpong]->draw(0,0,Aspect(@"Screen",1),1);
+                motionblurFbo[pingpong]->draw(0,0,Aspect(@"Screen",1),1);
                 
                 
-               // int i = [self getCurrentBufferIndex];    
+                // int i = [self getCurrentBufferIndex];    
                 //cout<<"Draw "<<i<<endl;
                 //history[i]->draw(0,0,Aspect(@"Screen",1),1);
-
-
+                
+                
             }
         } PopSurface();
         
         ofEnableAlphaBlending();
     }
     
-   /* if(PropB(@"useOpenCV")){
-        
-        ApplySurface(@"Screen");{
-            if([[GetPlugin(Kinect) enabled] boolValue] && [kinect kinectConnected]){
-                ofEnableAlphaBlending();
-                
-                
-                float scaleX = (1024.0/640);
-                float scaleY = (768.0/480);
-                
-                if(appliedProjector == 0){
-                    ofSetColor(255,255,255,PropF(@"frontAlpha")*255.0*powf(1- PropF(@"back"),1.0/2.2));                
-                } else {
-                    ofSetColor(255,255,255,255.0*powf(PropF(@"back"),1.0/2.2));                
-                }
-                
-                
-                
-                
-                ofxCvImage * img;
-                img = output;
-                
-                img->getTextureReference().bind();
-                glBegin(GL_QUADS);
-                glTexCoord2f(corners[0].x*scaleX, corners[0].y*scaleY);   glVertex2d(0, 0);
-                glTexCoord2f(corners[1].x*scaleX, corners[1].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 0);
-                glTexCoord2f(corners[2].x*scaleX, corners[2].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 1);
-                glTexCoord2f(corners[3].x*scaleX, corners[3].y*scaleY);   glVertex2d(0, 1);
-                glEnd();
-                img->getTextureReference().unbind();
-                
-                if(appliedProjector == 0){
-                    img = lightImage;
-                    float a = PropF(@"frontAlpha")*255.0*powf(1- PropF(@"back"),1.0/2.2);              
-                    ofSetColor(a,a,a, 255.0*(1-PropF(@"frontFakeShadow")));                
-                    
-                    
-                    img->getTextureReference().bind();
-                    glBegin(GL_QUADS);
-                    glTexCoord2f(corners[0].x*scaleX, corners[0].y*scaleY);   glVertex2d(0, 0);
-                    glTexCoord2f(corners[1].x*scaleX, corners[1].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 0);
-                    glTexCoord2f(corners[2].x*scaleX, corners[2].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 1);
-                    glTexCoord2f(corners[3].x*scaleX, corners[3].y*scaleY);   glVertex2d(0, 1);
-                    glEnd();
-                    img->getTextureReference().unbind();
-                }
-                
-            } 
-            
-            ofSetColor(255,255,255,255.0*PropF(@"flash"));
-            lightFlashImage->draw(0,0,Aspect(@"Screen",1),1);
-            
-            ofSetColor(255,255,255,255.0);
-            
-            
-        }PopSurface();
-        
-    }*/
-   
+    /* if(PropB(@"useOpenCV")){
+     
+     ApplySurface(@"Screen");{
+     if([[GetPlugin(Kinect) enabled] boolValue] && [kinect kinectConnected]){
+     ofEnableAlphaBlending();
+     
+     
+     float scaleX = (1024.0/640);
+     float scaleY = (768.0/480);
+     
+     if(appliedProjector == 0){
+     ofSetColor(255,255,255,PropF(@"frontAlpha")*255.0*powf(1- PropF(@"back"),1.0/2.2));                
+     } else {
+     ofSetColor(255,255,255,255.0*powf(PropF(@"back"),1.0/2.2));                
+     }
+     
+     
+     
+     
+     ofxCvImage * img;
+     img = output;
+     
+     img->getTextureReference().bind();
+     glBegin(GL_QUADS);
+     glTexCoord2f(corners[0].x*scaleX, corners[0].y*scaleY);   glVertex2d(0, 0);
+     glTexCoord2f(corners[1].x*scaleX, corners[1].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 0);
+     glTexCoord2f(corners[2].x*scaleX, corners[2].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 1);
+     glTexCoord2f(corners[3].x*scaleX, corners[3].y*scaleY);   glVertex2d(0, 1);
+     glEnd();
+     img->getTextureReference().unbind();
+     
+     if(appliedProjector == 0){
+     img = lightImage;
+     float a = PropF(@"frontAlpha")*255.0*powf(1- PropF(@"back"),1.0/2.2);              
+     ofSetColor(a,a,a, 255.0*(1-PropF(@"frontFakeShadow")));                
+     
+     
+     img->getTextureReference().bind();
+     glBegin(GL_QUADS);
+     glTexCoord2f(corners[0].x*scaleX, corners[0].y*scaleY);   glVertex2d(0, 0);
+     glTexCoord2f(corners[1].x*scaleX, corners[1].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 0);
+     glTexCoord2f(corners[2].x*scaleX, corners[2].y*scaleY);   glVertex2d(Aspect(@"Screen",1), 1);
+     glTexCoord2f(corners[3].x*scaleX, corners[3].y*scaleY);   glVertex2d(0, 1);
+     glEnd();
+     img->getTextureReference().unbind();
+     }
+     
+     } 
+     
+     ofSetColor(255,255,255,255.0*PropF(@"flash"));
+     lightFlashImage->draw(0,0,Aspect(@"Screen",1),1);
+     
+     ofSetColor(255,255,255,255.0);
+     
+     
+     }PopSurface();
+     
+     }*/
+    
 }
 
 
